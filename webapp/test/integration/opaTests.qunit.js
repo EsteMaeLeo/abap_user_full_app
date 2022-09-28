53317b4b3259619806a1118cca1ecf5faf95801e
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-09-28 18:22:47 -0500

    initial commit for SAPUI5 app

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["manageus/manageusers/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"manageus/manageusers/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
